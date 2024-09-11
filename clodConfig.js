2bcf04c0ff7d07fe619c7f3b31f5ed4aa084cfb0
Tarun Verma <166149739+Tarun-898@users.noreply.github.com>
2024-09-11 21:52:28 +0530

    Update clodConfig.js"testing complete"

--- clodConfig.js
+++ clodConfig.js
@@ -1,34 +1,11 @@
-// const cloudinary = require('cloudinary').v2;
-// const { CloudinaryStorage } = require('multer-storage-cloudinary');
-
-
-// cloudinary.config({
-//     cloud_name:process.env.ClOUD_NAME,
-//     api_key:process.env.CLOUD_API_KEY,
-//     api_secret:process.env.CLOUD_API_SECRET
-// });
-
-// const storage = new CloudinaryStorage({
-//     cloudinary: cloudinary,
-//     params: {
-//       folder: 'wanderlust',
-//       allowerdformat: async (req, file) => ["png","jpg","jpeg"], // supports promises as well
-//     //   public_id: (req, file) => 'computed-filename-using-request',
-//     },
-//   });
-
-// module.exports={
-//     cloudinary,
-//     storage,
-// };
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 
 cloudinary.config({
-    cloud_name:"dggntj69z",
-    api_key:"465565667873559",
-    api_secret:"ttIyqcN0eOGV15bmqiEnnwS9kNk"
+    cloud_name:process.env.ClOUD_NAME,
+    api_key:process.env.CLOUD_API_KEY,
+    api_secret:process.env.CLOUD_API_SECRET
 });
 
 const storage = new CloudinaryStorage({
@@ -44,3 +21,26 @@ module.exports={
     cloudinary,
     storage,
 };
+// const cloudinary = require('cloudinary').v2;
+// const { CloudinaryStorage } = require('multer-storage-cloudinary');
+
+
+// cloudinary.config({
+//     cloud_name:"dggntj69z",
+//     api_key:"465565667873559",
+//     api_secret:"ttIyqcN0eOGV15bmqiEnnwS9kNk"
+// });
+
+// const storage = new CloudinaryStorage({
+//     cloudinary: cloudinary,
+//     params: {
+//       folder: 'wanderlust',
+//       allowerdformat: async (req, file) => ["png","jpg","jpeg"], // supports promises as well
+//     //   public_id: (req, file) => 'computed-filename-using-request',
+//     },
+//   });
+
+// module.exports={
+//     cloudinary,
+//     storage,
+// };
